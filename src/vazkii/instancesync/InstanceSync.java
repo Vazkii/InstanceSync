cd0c9fb425e218b143eeb498f9a6086ca5aad14a
Jaret Varn <70246132+jav76@users.noreply.github.com>
2022-05-13 11:06:59 -0400

    Update InstanceSync.java
    Bump version for download URL fix

--- src/vazkii/instancesync/InstanceSync.java
+++ src/vazkii/instancesync/InstanceSync.java
@@ -8,7 +8,7 @@ import com.google.gson.Gson;
 
 public final class InstanceSync {
 	
-	private static final String VERSION = "1.0.2";
+	private static final String VERSION = "1.0.3";
 
 	public static void main(String[] args) {
 		System.out.println("InstanceSync " + VERSION);
